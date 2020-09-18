f2e756c657d9e7d35fc247921b78b2a267a73076
conner fortner <fortner.conner@yahoo.com>
2020-09-18 01:41:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,7 +23,7 @@ def adding
                   "oil paintings" => 3
                  }
   shipping_manifest = {"muskets" => 2, "gun powder" => 4}
-
+return shipping_manifest
 
 
   # add 4 gun powder to the shipping_manifest hash below
