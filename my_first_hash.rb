4e5462ca5625a4adf14867800558b79d6bfadfcf
conner fortner <fortner.conner@yahoo.com>
2020-09-18 01:34:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,7 +5,7 @@ end
 
 
 def shipping_manifest
-    the_manifest = {"whale bone corsets" => "5", "porcelain vases" => "2", "oil paintings" => "3"}
+    the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
 
 end
@@ -22,7 +22,7 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest = {"muskets" => "2", "gun powder" => "4"}
+  shipping_manifest = {"muskets" => 2, "gun powder" => 4}
 
 
 
